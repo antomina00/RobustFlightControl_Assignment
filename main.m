8893a84e9437e4b3f9c6471f3ac170d09299c5bb
PRM28 <P.RinaldiMeneses@student.tudelft.nl>
2024-06-09 19:11:26 +0200

    added correct names to input,state,output of both models and created the simulink open loop model of the system + actuator dynamics

--- main.m
+++ main.m
@@ -11,7 +11,28 @@ A_alpha  = 1434.7783; % m/s^2*rad normal acc derivative
 A_delta = 115.0529; %m/s^2*rad control acc derivative
 omega_a = 150; % rad/s actuator nat freq
 zeta_a = 0.7; % actuator damping ratio	 
+V = M*a; % m/s velocity at operating point
 
 %% Defining the A, B, C, D matrices for the short period model of the system as well as the state space model
 
+A_sp = [-Z_alpha/V, 1.0; M_alpha, M_q];
+B_sp = [-Z_delta/V; M_delta];
+C_sp = [-A_alpha/g, 0.0; 0.0, 1];
+D_sp = [-A_delta/g; 0.0];
+
+G_m = ss(A_sp, B_sp, C_sp, D_sp);
+G_m.InputName = 'u_m';
+G_m.StateName = {'x1', 'x2'};
+G_m.OutputName = {'y1', 'y2'};
+
 %% Defining the A, B, C, D matrices for the actuator model of the system as well as its state space model
+
+A_act = [0.0, 1.0; -omega_a^2, -2*zeta_a*omega_a];
+B_act = [0.0; omega_a^2];
+C_act = [1.0, 0.0; 0.0, 1.0];
+D_act = [0.0; 0.0];
+
+G_act = ss(A_act, B_act, C_act, D_act); 
+G_act.InputName = 'u_cmd';
+G_act.StateName = {'x3', 'x4'};
+G_act.OutputName = {'u_m', 'udot_m'};
